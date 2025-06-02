ead67cf4c4cbcc4389cf5491f790b9d731679d30
MatheusMoraes13 <profissional.moraes2004@gmail.com>
2025-06-02 20:20:56 -0300

    Update: criando o a estrutura de repositórios do projeto

---
+++ back-end/agropec/src/main/java/br/com/o_tab/agropec/AgropecApplication.java
@@ -0,0 +1,13 @@
+package br.com.o_tab.agropec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgropecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgropecApplication.class, args);
+	}
+
+}
