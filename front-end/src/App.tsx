a2c064cde15ce5292e23779395d64ea5a5c11f9d
L1m4z1n <144862058+L1m4z1n@users.noreply.github.com>
2025-06-15 17:38:11 -0300

    Adicionando componente Footer em todas as páginas

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+// Pages
 import Mainpage from './pages/Mainpage';
 import Aboutpage from './pages/Aboutpage';
 import StandsPage from './pages/StandsPage';
 import MapPage from "./pages/MapPage";
 
-// components
+// Components
 import NavbarComponents from './components/NavbarComponents';
-
-
+import Footer from '../src/components/Footer';
 
 function App() {
-  
   return (
-    <main className="App">
-      <NavbarComponents/>
-      <Routes>
-        {/* Define suas rotas aqui */}
-        <Route path="/" element={<Mainpage/>} />
-        <Route path="/about" element={<Aboutpage/>} />
-        <Route path="/stands" element={<StandsPage/>} />
-        <Route path="/events" element={<h1>Programações</h1>} />
-        <Route path="/map" element={<MapPage />} />
-      </Routes>
-    </main>
+    <main className="App flex flex-col min-h-screen">
+      <NavbarComponents />
 
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Mainpage />} />
+          <Route path="/about" element={<Aboutpage />} />
+          <Route path="/stands" element={<StandsPage />} />
+          <Route path="/events" element={<h1>Programações</h1>} />
+          <Route path="/map" element={<MapPage />} />
+        </Routes>
+      </div>
+
+      <Footer />
+    </main>
   );
 }
 
